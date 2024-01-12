73ab16a05b7f63d21f42d226f4f9b50924f2581d
StevenAwo <rotimiawomodu@gmail.com>
2024-01-12 01:03:19 +0100

    TASK 5

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -28,13 +28,21 @@ package { 'nginx':
 
 } ->
 
-
 file { '/data':
   ensure  => 'directory'
 
 } ->
 
+file { '/var/www':
+  ensure => 'directory'
+
+} ->
+
 file { '/data/web_static':
+  ensure => 'directory'
+
+} ->
+file { '/var/www/html':
   ensure => 'directory'
 
 } ->
@@ -44,19 +52,31 @@ file { '/data/web_static/releases':
 
 } ->
 
+file { '/var/www/html/index.html':
+  ensure  => 'present',
+  content => "Holberton School Nginx\n"
+
+} ->
+
 file { '/data/web_static/releases/test':
   ensure => 'directory'
 
 } ->
 
-file { '/data/web_static/shared':
-  ensure => 'directory'
+file { '/var/www/html/404.html':
+  ensure  => 'present',
+  content => "Ceci n'est pas une page\n"
 
 } ->
 
 file { '/data/web_static/releases/test/index.html':
   ensure  => 'present',
   content => "Holberton School Puppet\n"
+
+} ->
+
+file { '/data/web_static/shared':
+  ensure => 'directory'
 
 } ->
 
@@ -69,28 +89,6 @@ file { '/data/web_static/current':
 exec { 'chown -R ubuntu:ubuntu /data/':
   path => '/usr/bin/:/usr/local/bin/:/bin/'
 }
-
-file { '/var/www':
-  ensure => 'directory'
-
-} ->
-
-file { '/var/www/html':
-  ensure => 'directory'
-
-} ->
-
-file { '/var/www/html/index.html':
-  ensure  => 'present',
-  content => "Holberton School Nginx\n"
-
-} ->
-
-file { '/var/www/html/404.html':
-  ensure  => 'present',
-  content => "Ceci n'est pas une page\n"
-
-} ->
 
 file { '/etc/nginx/sites-available/default':
   ensure  => 'present',
