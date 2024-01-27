3022c571fc777ee70ce561bb30c616ddc42ed486
StevenAwo <rotimiawomodu@gmail.com>
2024-01-27 11:07:18 +0100

    TASK 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+Starting a web application that has two routings
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    """
+    Returning the string to when the route queried
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """
+    Returning the string to when the route queried
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_is_fun(text):
+    """
+    Returning the reformatted texts
+    """
+    return 'C ' + text.replace('_', ' ')
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def python_with_text(text='is cool'):
+    """Reformatting the text thats based on the optional variable
+    """
+    return 'Python ' + text.replace('_', ' ')
+
+if __name__ == '__main__':
+    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)
