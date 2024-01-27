7ecfb161a2613820c4385ac6ac15fa4404743918
StevenAwo <rotimiawomodu@gmail.com>
2024-01-27 10:49:16 +0100

    TASK 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Starting a flask's web app
+"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    """
+    Returning the string to when the route queried
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    """
+    Returning the string to when the route queried
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_is_fun(text):
+    """
+    Returning the reformatted texts
+    """
+    return 'C ' + text.replace('_', ' ')
+
+if __name__ == '__main__':
+    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0', port=5000)
