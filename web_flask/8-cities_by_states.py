cfe3ec3c7cd28d1cf0012dd6544152c02cc62cd5
StevenAwo <rotimiawomodu@gmail.com>
2024-01-28 17:33:52 +0100

    TASK 9

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,28 +1,30 @@
 #!/usr/bin/python3
-"""Start web application with two routings
+"""
+Starting the web's application with just two routings
 """
 
-from models import storage
 from models.state import State
+
+from models import storage
+
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 
-@app.route('/cities_by_states')
+@app.route('/states_list')
 def states_list():
-    """Render template with states
+    """
+    Rendering the template with the states
     """
     path = '8-cities_by_states.html'
     states = storage.all(State)
-
-    # sort State object alphabetically by name
-    # sorted_states = sorted(states.values(), key=lambda state: state.name)
-    return render_template(path, states=states)
+    return render_template(path, states=states))
 
 
 @app.teardown_appcontext
 def app_teardown(arg=None):
-    """Clean-up session
+    """Cleaning-up the session
     """
     storage.close()
 
@@ -30,3 +32,4 @@ def app_teardown(arg=None):
 if __name__ == '__main__':
     app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
+
